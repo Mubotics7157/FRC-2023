6d2e614f2e73896cff9fdbad35862944c4b04a10
Susan Yoon <97490106+awesomeyooner@users.noreply.github.com>
2023-01-10 17:56:53 -0800

    added apriltag locations

--- frc2023/src/main/java/frc/robot/Constants.java
+++ frc2023/src/main/java/frc/robot/Constants.java
@@ -4,11 +4,16 @@
 
 package frc.robot;
 
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import frc.robot.util.SwerveModule;
 
 public final class Constants {
@@ -84,6 +89,80 @@ public static class DriveConstants{
 
   public static final SwerveModuleState[] LOCKED_SwerveModule_STATES = {new SwerveModuleState(0, Rotation2d.fromDegrees(45)),new SwerveModuleState(0, Rotation2d.fromDegrees(-45)),new SwerveModuleState(0, Rotation2d.fromDegrees(-45)),new SwerveModuleState(0, Rotation2d.fromDegrees(45))};
 
+}
+
+public static class FieldConstants{
+  public static final AprilTag tag01= new AprilTag(
+    1,
+          new Pose3d(
+              Units.inchesToMeters(610.77),
+              Units.inchesToMeters(42.19),
+              Units.inchesToMeters(18.22),
+              new Rotation3d(0.0, 0.0, Math.PI))
+  );
+
+  public static final AprilTag tag02 = new AprilTag(
+    2,
+          new Pose3d(
+              Units.inchesToMeters(610.77),
+              Units.inchesToMeters(108.19),
+              Units.inchesToMeters(18.22),
+              new Rotation3d(0.0, 0.0, Math.PI))
+  );
+
+  public static final AprilTag tag03 = new AprilTag(
+    3,
+          new Pose3d(
+              Units.inchesToMeters(610.77),
+              Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
+              Units.inchesToMeters(18.22),
+              new Rotation3d(0.0, 0.0, Math.PI))
+  );
+
+  public static final AprilTag tag04 = new AprilTag(
+    4,
+    new Pose3d(
+        Units.inchesToMeters(636.96),
+        Units.inchesToMeters(265.74),
+        Units.inchesToMeters(27.38),
+        new Rotation3d(0.0, 0.0, Math.PI))
+  );
+
+  public static final AprilTag tag05 = new AprilTag(
+    5,
+          new Pose3d(
+              Units.inchesToMeters(14.25),
+              Units.inchesToMeters(265.74),
+              Units.inchesToMeters(27.38),
+              new Rotation3d())
+  );
+
+  public static final AprilTag tag06 = new AprilTag(
+    6,
+    new Pose3d(
+        Units.inchesToMeters(40.45),
+        Units.inchesToMeters(174.19), // FIRST's diagram has a typo (it says 147.19)
+        Units.inchesToMeters(18.22),
+        new Rotation3d())
+  );
+
+  public static final AprilTag tag07 = new AprilTag(
+    7,
+          new Pose3d(
+              Units.inchesToMeters(40.45),
+              Units.inchesToMeters(108.19),
+              Units.inchesToMeters(18.22),
+              new Rotation3d())
+  );
+
+  public static final AprilTag tag08 = new AprilTag(
+    8,
+          new Pose3d(
+              Units.inchesToMeters(40.45),
+              Units.inchesToMeters(42.19),
+              Units.inchesToMeters(18.22),
+              new Rotation3d())
+  );
 }
 
 }
