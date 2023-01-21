1b78579dc736b61de47c36b21d94d5fa5b85e35e
Susan Yoon <97490106+awesomeyooner@users.noreply.github.com>
2023-01-21 13:19:28 -0800

    updated vision for network tables 4

--- sparkMaxDriveTrain/src/main/java/frc/robot/subsystems/VisionManager.java
+++ sparkMaxDriveTrain/src/main/java/frc/robot/subsystems/VisionManager.java
@@ -22,19 +22,19 @@ public class VisionManager extends SubsystemBase{
         return instance;
     }
 
-    public Rotation2d getTargetYaw(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        double yaw = tableLime.getEntry("tx").getDouble(0);
+    public double getTargetYaw(){
+        double targets = tableLime.getDoubleTopic("tv").subscribe(0).get();
+        double yaw = tableLime.getDoubleTopic("tx").subscribe(0).get();
         if(targets != 0){
-            return Rotation2d.fromDegrees(yaw);
+            return yaw;
         }
         else
-            return Rotation2d.fromDegrees(0);
+            return 0;
     }
 
     public double getTargetPitch(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        double pitch = tableLime.getEntry("ty").getDouble(0);
+        double targets = tableLime.getDoubleTopic("tv").subscribe(0).get();
+        double pitch = tableLime.getDoubleTopic("ty").subscribe(0).get();
         if(targets != 0){
             return pitch;
         }
@@ -43,12 +43,12 @@ public class VisionManager extends SubsystemBase{
     }
 
     public double getTargets(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
+        double targets = tableLime.getDoubleTopic("tv").subscribe(0).get();
         return targets;
     }
 
     public boolean hasTargets(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
+        double targets = tableLime.getDoubleTopic("tv").subscribe(0).get();
 
         if(targets == 0){
             return false;
@@ -58,26 +58,26 @@ public class VisionManager extends SubsystemBase{
     }
 
     public void toggleLimeLight(){
-        if(tableLime.getEntry("ledMode").getDouble(0) == 1){
-            tableLime.getEntry("ledMode").setNumber(1);
+        if(tableLime.getDoubleTopic("ledMode").subscribe(0).get() == 1){
+            tableLime.getDoubleTopic("ledMode").publish().set(0);
         }
-        else if(tableLime.getEntry("ledMode").getDouble(0) == 0){
-            tableLime.getEntry("ledMode").setNumber(1);
+        else if(tableLime.getDoubleTopic("ledMode").subscribe(0).get() == 0){
+            tableLime.getDoubleTopic("ledMode").publish().set(1);
         }
     }
 
     public void togglePipeLine(){
         //switch to object detection to reflective tape
-        if(tableLime.getEntry("pipeline").getDouble(0) == 1){
-            tableLime.getEntry("pipeline").setDouble(0);
+        if(tableLime.getDoubleTopic("pipeline").subscribe(0).get() == 1){
+            tableLime.getDoubleTopic("pipeline").publish().set(0);;
         }
-        else if(tableLime.getEntry("pipeline").getDouble(0) == 0){
-            tableLime.getEntry("pipeline").setDouble(1);
+        else if(tableLime.getDoubleTopic("pipeline").subscribe(0).get() == 0){
+            tableLime.getDoubleTopic("pipeline").publish().set(1);
         }
     }
     
     public void logData(){
-        SmartDashboard.putNumber("Target yaw", getTargetYaw().getDegrees());
+        SmartDashboard.putNumber("Target yaw", getTargetYaw());
         SmartDashboard.putNumber("Target pitch", getTargetPitch());
         SmartDashboard.putNumber("Targets", getTargets());
     
@@ -85,7 +85,7 @@ public class VisionManager extends SubsystemBase{
 
     public Pose2d getBotPose(){
         /* 
-        double[] poseEntry = tableLime.getEntry("botpose").getDoubleArray(testArray);
+        double[] poseEntry = tableLime.getDoubleTopic("botpose").getDoubleArray(testArray);
 
         if(hasTargets()&& poseEntry.length>0){
         
@@ -97,7 +97,7 @@ public class VisionManager extends SubsystemBase{
             */
         if(hasTargets()){
             try{
-                double[] poseEntry = tableLime.getEntry("botpose").getDoubleArray(testArray);
+                double[] poseEntry = tableLime.getDoubleArrayTopic("botpose").subscribe(testArray).get();
                 Pose2d pose = new Pose2d(poseEntry[0], poseEntry[1], Rotation2d.fromDegrees(poseEntry[5]));
                 return pose;
             }
@@ -110,7 +110,7 @@ public class VisionManager extends SubsystemBase{
     }
 
     public double getLatency(){
-        double latency = tableLime.getEntry("tl").getDouble(0);
+        double latency = tableLime.getDoubleTopic("tl").subscribe(0).get();
         return latency;
     }
 }   
