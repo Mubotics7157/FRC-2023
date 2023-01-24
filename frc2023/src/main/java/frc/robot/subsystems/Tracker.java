79c6d94f84a8df96722872fbdb69a6a42766310b
Susan Yoon <97490106+awesomeyooner@users.noreply.github.com>
2023-01-23 17:55:39 -0800

    updated vision for photon vision and also pose estimator

--- frc2023/src/main/java/frc/robot/subsystems/Tracker.java
+++ frc2023/src/main/java/frc/robot/subsystems/Tracker.java
@@ -2,29 +2,57 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.math.MatBuilder;
+import edu.wpi.first.math.Nat;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.SwerveModuleConstants;
 
 public class Tracker extends SubsystemBase{
 
+    SwerveDrivePoseEstimator estimator = new SwerveDrivePoseEstimator(DriveConstants.DRIVE_KINEMATICS, Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions(), new Pose2d());
+
     SwerveDriveOdometry odometry = new SwerveDriveOdometry(DriveConstants.DRIVE_KINEMATICS, Drive.getInstance().getDriveHeading(),Drive.getInstance().getModulePositions());
+
     private final Field2d m_field = new Field2d();
 
     private static Tracker instance = new Tracker();
 
     public Tracker(){
         SmartDashboard.putData("Field", m_field);
+        
+        estimator.setVisionMeasurementStdDevs(
+            new MatBuilder<>(Nat.N3(), Nat.N1()).fill(
+                0.02, //x 
+                0.02, //y
+                0.01  //theta
+            ));
     }
 
+    public synchronized void updatePose(){
+        if(VisionManager.getInstance().limeHasTargets())
+            estimator.addVisionMeasurement(VisionManager.getInstance().getLimePose(), VisionManager.getInstance().getLimeLatency());   
+        
+        estimator.update(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions());
+    }
 
-
+    public synchronized void adjustDeviation(double x, double y, double r){
+        estimator.setVisionMeasurementStdDevs(
+            new MatBuilder<>(Nat.N3(), Nat.N1()).fill(
+                x, //x 
+                y, //y
+                r  //theta
+        ));
+    }
 
     public static Tracker getInstance(){
         return instance;
@@ -32,11 +60,17 @@ public class Tracker extends SubsystemBase{
 
     @Override
     public void periodic() {
-         m_field.setRobotPose(odometry.getPoseMeters());
-         odometry.update(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions());
-         SmartDashboard.putNumber("odom x", getOdometry().getX());
-         SmartDashboard.putNumber("odom y", getOdometry().getY());
+        m_field.setRobotPose(estimator.getEstimatedPosition());
+        odometry.update(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions());
+        updatePose();
+
+        SmartDashboard.putNumber("odom x", getOdometry().getX());
+        SmartDashboard.putNumber("odom y", getOdometry().getY());
         SmartDashboard.putNumber("odometry r", getOdometry().getRotation().getDegrees());
+
+        SmartDashboard.putNumber("estim x", getPose().getX());
+        SmartDashboard.putNumber("estim y", getPose().getY());
+        SmartDashboard.putNumber("estim r", getPose().getRotation().getDegrees());
     }
 
     public synchronized void setOdometry(Pose2d pose){
@@ -47,10 +81,23 @@ public class Tracker extends SubsystemBase{
         return odometry.getPoseMeters();
     }
 
-
     public synchronized void resetHeading(){
         odometry.resetPosition(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions(), new Pose2d(odometry.getPoseMeters().getTranslation(), Rotation2d.fromDegrees(0)));
     }
+
+    public synchronized Pose2d getPose(){
+        return estimator.getEstimatedPosition();
+    }
+
+    public synchronized void setPose(Pose2d pose){
+        estimator.resetPosition(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions(), pose);
+    }
+
+    public synchronized void resetPoseHeading(){
+        estimator.resetPosition(Drive.getInstance().getDriveHeading(), Drive.getInstance().getModulePositions(), new Pose2d(estimator.getEstimatedPosition().getTranslation(), Rotation2d.fromDegrees(0)));
+    }
+
+
 
 
 
--- frc2023/src/main/java/frc/robot/subsystems/VisionManager.java
+++ frc2023/src/main/java/frc/robot/subsystems/VisionManager.java
@@ -1,86 +1,157 @@
 package frc.robot.subsystems;
 
-import org.photonvision.PhotonUtils;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionManager extends SubsystemBase{
+    private static VisionManager instance = new VisionManager();
+
+    PhotonCamera limeLight;
+    PhotonPipelineResult limeResult;
+
+    PhotonCamera shutter;
+    PhotonPipelineResult shutterResult;
     
-    NetworkTable tableLime;
+    AprilTagFieldLayout aprilTagFieldLayout;
+
+    Transform3d robotToLime;
+    Transform3d robotToShutter;
+
+    PhotonPoseEstimator limePoseEstimator;
+    PhotonPoseEstimator shutterPoseEstimator;
+    
 
     public VisionManager(){
-        tableLime = NetworkTableInstance.getDefault().getTable("limelight");
+        limeLight = new PhotonCamera("limeLight");
+        limeResult = limeLight.getLatestResult();
+        
+        shutter = new PhotonCamera("GSC");
+        shutterResult = shutter.getLatestResult();
+
+        try{
+        aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2023ChargedUp.m_resourceFile);
+        }
+        catch(Exception e){
+            System.out.print("yes");
+        }
+
+        robotToShutter = new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0, 0, 0));
+        robotToLime = new Transform3d(new Translation3d(0.0, 0.0, 0.0), new Rotation3d(0,0,0));
+        //^^^^^ needs to be configured!! this is the distance of your camera to the center of your robobot >:P 
+        limePoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE, limeLight, robotToLime);
+        shutterPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.CLOSEST_TO_REFERENCE_POSE,shutter, robotToShutter);
+
     }
 
-    public Rotation2d getTargetYaw(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        double yaw = tableLime.getEntry("tx").getDouble(0);
-        if(targets != 0){
-            return Rotation2d.fromDegrees(yaw);
+    @Override
+    public void periodic() {
+        updateResults();
+    }
+
+    public static VisionManager getInstance(){
+        return instance;
+    }
+
+    public boolean limeHasTargets(){
+        boolean hasTargets = limeResult.hasTargets();
+        return hasTargets;
+    }
+
+    public boolean shutterHasTargets(){
+        boolean hasTargets = shutterResult.hasTargets();
+        return hasTargets;
+    }
+
+    public Rotation2d getLimeYaw(){
+        if(limeHasTargets()){
+            return Rotation2d.fromDegrees(limeResult.getBestTarget().getYaw());
         }
         else
             return Rotation2d.fromDegrees(0);
     }
 
-    public double getPitch(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        double pitch = tableLime.getEntry("ty").getDouble(0);
-        if(targets != 0){
-            return pitch;
+    public Rotation2d getShutterYaw(){
+        if(shutterHasTargets()){
+            return Rotation2d.fromDegrees(shutterResult.getBestTarget().getYaw());
         }
         else
-            return 0;
+            return Rotation2d.fromDegrees(0);
     }
 
-    public double getTargets(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        return targets;
-    }
-
-    public boolean hasTargets(){
-        double targets = tableLime.getEntry("tv").getDouble(0);
-        if(targets != 0){
-            return false;
+    public Rotation2d getLimePitch(){
+        if(limeHasTargets()){
+            return Rotation2d.fromDegrees(limeResult.getBestTarget().getPitch());
         }
         else
-            return true;
+            return Rotation2d.fromDegrees(0);
+    } 
+
+    public Rotation2d getShutterPitch(){
+        if(shutterHasTargets()){
+            return Rotation2d.fromDegrees(shutterResult.getBestTarget().getPitch());
+            }
+        else
+            return Rotation2d.fromDegrees(0);
     }
 
-    public void toggleLimeLight(){
-        if(tableLime.getEntry("ledMode").getDouble(0) == 1){
-            tableLime.getEntry("ledMode").setNumber(1);
-        }
-        else if(tableLime.getEntry("ledMode").getDouble(0) == 0){
-            tableLime.getEntry("ledMode").setNumber(1);
-        }
+    public double getLimeLatency(){
+        return limeResult.getTimestampSeconds();
+    }
+
+    public double getShutterLatency(){
+        return shutterResult.getTimestampSeconds();
+    }
+
+    public Optional<EstimatedRobotPose> getEstimatedLimePose(Pose2d prevEstimatedRobotPose) {
+        limePoseEstimator.setReferencePose(prevEstimatedRobotPose);
+        return limePoseEstimator.update();
+    }
+
+    public Optional<EstimatedRobotPose> getEstimatedShutterPose(Pose2d prevEstimatedRobotPose) {
+        shutterPoseEstimator.setReferencePose(prevEstimatedRobotPose);
+        return shutterPoseEstimator.update();
+    }
+
+    public void updateResults(){
+        limeResult = limeLight.getLatestResult();
+        shutterResult = shutter.getLatestResult();
+    }
+
+    public Pose2d getLimePose(){
+        EstimatedRobotPose estPose = getEstimatedLimePose(Tracker.getInstance().getOdometry()).get();
+        double x = estPose.estimatedPose.getX();
+        double y = estPose.estimatedPose.getY();
+        Rotation2d theta = Rotation2d.fromRadians(estPose.estimatedPose.getRotation().getAngle());
+
+        Pose2d pose = new Pose2d(x, y, theta);
+        return pose;
+    }
+
+    public Pose2d getShutterPose(){
+        EstimatedRobotPose estPose = getEstimatedShutterPose(Tracker.getInstance().getOdometry()).get();
+        double x = estPose.estimatedPose.getX();
+        double y = estPose.estimatedPose.getY();
+        Rotation2d theta = Rotation2d.fromRadians(estPose.estimatedPose.getRotation().getAngle());
+
+        Pose2d pose = new Pose2d(x, y, theta);
+        return pose;
     }
 
 
-    public void togglePipeLine(){
-        //switch to object detection to reflective tape
-        if(tableLime.getEntry("pipeline").getDouble(0) == 1){
-            tableLime.getEntry("pipeline").setDouble(0);
-        }
-        else if(tableLime.getEntry("pipeline").getDouble(0) == 0){
-            tableLime.getEntry("pipeline").setDouble(1);
-        }
-    }
-    
-    public void logData(){
-        SmartDashboard.putNumber("Target yaw", getTargetYaw().getDegrees());
-        SmartDashboard.putNumber("Target pitch", getPitch());
-        SmartDashboard.putNumber("Targets", getTargets());
-    
-    }
-    public double getPoleOffset(){
-        //gets the offset of the pole to the game object on the intake (to account for error)
-        return 0;
-    }
-}
+
+}   
