3265cbc375032fcdd268d56db8c16bafeb096667
Susan Yoon <97490106+awesomeyooner@users.noreply.github.com>
2023-04-09 18:46:10 -0700

    4/9 checkpoint
    Co-Authored-By: Veronica Diaz <60453038+Vernonicuhhh@users.noreply.github.com>

--- beta-bot/beta-bot/src/main/java/frc/robot/commands/auto/PreloadPlusTwoWeak.java
+++ beta-bot/beta-bot/src/main/java/frc/robot/commands/auto/PreloadPlusTwoWeak.java
@@ -28,10 +28,10 @@ import frc.robot.subsystems.VisionManager.VisionState;
 public class PreloadPlusTwoWeak extends SequentialCommandGroup{
 
     public PreloadPlusTwoWeak(Drive drive,VisionManager vision, SuperStructure superStructure,Tracker tracker){
-        PathPlannerTrajectory driveToCube = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart1", 3,3);
+        PathPlannerTrajectory driveToCube = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart1", 2,3);
         PathPlannerTrajectory driveToCubeNodeOne = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart2",3,4);
-        PathPlannerTrajectory driveToCubeTwo = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart3", 3, 3);
-        PathPlannerTrajectory driveToCubeNodeTwo = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart4", 1,1  );
+        PathPlannerTrajectory driveToCubeTwo = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart3", 2.5, 3);
+        PathPlannerTrajectory driveToCubeNodeTwo = PathPlanner.loadPath("PreloadPlusTwoWeakSidePart4", 3,4);
 
 
         addCommands(
@@ -41,11 +41,11 @@ public class PreloadPlusTwoWeak extends SequentialCommandGroup{
          new ShootCone(),
          new WaitCommand(.2),
          new Stow(superStructure),
-         new ParallelCommandGroup(drive.followPath(driveToCube,true),new SequentialCommandGroup(new WaitCommand(2.55),new SetVisionMode(vision, VisionState.CUBE),new SetIntakingHeight(superStructure, SuperStructureState.CUBE_INTAKE)
+         new ParallelCommandGroup(drive.followPath(driveToCube,true),new SequentialCommandGroup(new WaitCommand(2.25),new SetVisionMode(vision, VisionState.CUBE),new SetIntakingHeight(superStructure, SuperStructureState.CUBE_INTAKE)
          )).andThen(new ParallelCommandGroup(new AlignObject(drive, vision, PathPlannerTrajectory.transformTrajectoryForAlliance(driveToCubeNodeOne, DriverStation.getAlliance()).getInitialHolonomicPose()))
          /*new DriveBackwards( .85, drive, tracker,PathPlannerTrajectory.transformTrajectoryForAlliance(driveToCubeNodeOne, DriverStation.getAlliance()).getInitialHolonomicPose(),1.5)*/),
         // new SetVisionMode(vision, VisionState.TAG),
-         new ParallelCommandGroup(drive.followPath(driveToCubeNodeOne,false), new SequentialCommandGroup(new WaitCommand(1), new ConeSniper(superStructure), new WaitCommand(.5), new ShootCone())),
+         new ParallelCommandGroup(drive.followPath(driveToCubeNodeOne,false), new SequentialCommandGroup(new WaitCommand(1), new ConeSniper(superStructure), new WaitCommand(.6), new ShootCone())),
          //new SequentialCommandGroup(new ScoreCubeHigh(superStructure), new ShootCone(), new WaitCommand(.6), new Stow(superStructure)),
          new ParallelCommandGroup(drive.followPath(driveToCubeTwo, false),new SequentialCommandGroup(new WaitCommand(.25), new SetVisionMode(vision,VisionState.CUBE).andThen(new SetIntakingHeight(superStructure, SuperStructureState.CUBE_INTAKE)))),
          new AlignObject(drive, vision, new Pose2d(driveToCubeNodeTwo.getInitialHolonomicPose().getTranslation(), Tracker.getInstance().getPose().getRotation())),
@@ -54,7 +54,12 @@ public class PreloadPlusTwoWeak extends SequentialCommandGroup{
          //
          //new SetVisionMode(vision, VisionState.TAG),
          //new Stow(superStructure),  
-         new ParallelCommandGroup(drive.followPath(driveToCubeNodeTwo, false),new SequentialCommandGroup(new WaitCommand(3),new Stow(superStructure)))
+         new ParallelCommandGroup(drive.followPath(driveToCubeNodeTwo, false),new SequentialCommandGroup(new WaitCommand(1),new Stow(superStructure), new SetVisionMode(vision, VisionState.TAG))),
+         new DriveSlow(0.3, drive, tracker),
+         new ScoreCubeHigh(superStructure),
+         new ShootCone(),
+         new WaitCommand(0.4),
+         new Stow(superStructure)
         );
     }
 
