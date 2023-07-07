5eca597ec4973e902cb12a1f5bb7ffb77ab0dd83
Susan Yoon <97490106+awesomeyooner@users.noreply.github.com>
2023-07-06 18:36:18 -0700

    cleaned up super structure a tad bit

--- beta-bot/beta-bot/src/main/java/frc/robot/subsystems/SuperStructure.java
+++ beta-bot/beta-bot/src/main/java/frc/robot/subsystems/SuperStructure.java
@@ -18,8 +18,7 @@ public class SuperStructure extends SubsystemBase {
     private Elevator elevator = Elevator.getInstance();
     private Wrist wrist = Wrist.getInstance();
     private LED led = LED.getInstance();
-    //private LED led;
-    private boolean scoreHigh;
+   
     private boolean idleIntake = true;
     
     private SuperStructureState scoringState = SuperStructureState.STOWED;
@@ -61,8 +60,6 @@ public class SuperStructure extends SubsystemBase {
     public SuperStructure(){
         SmartDashboard.putNumber("custom wrist adjustment", 0);
         SmartDashboard.putNumber("custom elevator adjustment", 0);
-        //led = LED.getInstance();
-
     }
 
     @Override
@@ -141,9 +138,7 @@ public class SuperStructure extends SubsystemBase {
     }
 
     public void zeroAll(){
-        //elevator.setState(ElevatorState.ZERO);
         wrist.setWristState(WristState.ZERO);
-        //intake.setIntakeState(IntakeState.OFF);
     }
 
     public boolean isZeroed(){
@@ -163,97 +158,128 @@ public class SuperStructure extends SubsystemBase {
     }
 
     public void setState(SuperStructureState state){
-    wristAdj = Rotation2d.fromDegrees(-6).plus(Rotation2d.fromDegrees(SmartDashboard.getNumber("custom wrist adjustment", 0)));
-    elevAdj = SmartDashboard.getNumber("custom elevator adjustment", 0);
+        wristAdj = Rotation2d.fromDegrees(-6).plus(Rotation2d.fromDegrees(SmartDashboard.getNumber("custom wrist adjustment", 0)));
+        elevAdj = SmartDashboard.getNumber("custom elevator adjustment", 0);
+
 
     if(state==SuperStructureState.CUBE_INTAKE || state==SuperStructureState.CONE_INTAKE|| state ==SuperStructureState.FALLEN_CONE)
         idleIntake = false;
-    else if((state==SuperStructureState.CONE_HIGH || state==SuperStructureState.CONE_MID || state==SuperStructureState.CONE_MID_SUPER || scoringState == SuperStructureState.FALLEN_CONE || scoringState == SuperStructureState.CONE_INTAKE || scoringState == SuperStructureState.PORTAL || scoringState == SuperStructureState.SEAGUL || state == SuperStructureState.CONE_SNIPER) || !atSetpoint())
+
+    else if((state==SuperStructureState.CONE_HIGH || 
+                state==SuperStructureState.CONE_MID || 
+                state==SuperStructureState.CONE_MID_SUPER || 
+                scoringState == SuperStructureState.FALLEN_CONE || 
+                scoringState == SuperStructureState.CONE_INTAKE || 
+                scoringState == SuperStructureState.PORTAL || 
+                scoringState == SuperStructureState.SEAGUL || 
+                state == SuperStructureState.CONE_SNIPER) || 
+                !atSetpoint())
         idleIntake = true;
+
     else
         idleIntake = false;
-    
-    
+
+        
     
         scoringState = state;
+
         setLedMode(scoringState);
 
         switch(scoringState){
+
             case CONE_HIGH:
                 goToPosition(SuperStructureConstants.ELEVATOR_CONE_HIGH + elevAdj, SuperStructureConstants.WRIST_CONE_HIGH.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CONE_MID:
                 goToPosition(SuperStructureConstants.ELEVATOR_CONE_MID + elevAdj, SuperStructureConstants.WRIST_CONE_MID.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
                 case CONE_MID_SUPER:
                 goToPosition(SuperStructureConstants.ELEVATOR_CONE_MID_SUPER+ elevAdj, SuperStructureConstants.WRIST_CONE_MID_SUPER.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CUBE_HIGH:
                 goToPosition(SuperStructureConstants.ELEVATOR_CUBE_HIGH + elevAdj, SuperStructureConstants.WRIST_CUBE_HIGH.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CUBE_MID:
                 goToPosition(SuperStructureConstants.ELEVATOR_CUBE_MID + elevAdj, SuperStructureConstants.WRIST_CUBE_MID.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CUBE_HIGH_SHOOT:
                 goToPosition(SuperStructureConstants.ELEVATOR_CUBE_SHOOT + elevAdj, SuperStructureConstants.WRIST_CUBE_SHOOT.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CUBE_MID_SHOOT:
                 goToPosition(SuperStructureConstants.ELEVATOR_CUBE_SHOOT + elevAdj, SuperStructureConstants.WRIST_CUBE_SHOOT.plus(wristAdj));
                 Drive.getInstance().changeSlow();
                 break;
+
             case CUBE_HYBRID:
                 goToPosition(SuperStructureConstants.ELEVATOR_CUBE_HYBRID + elevAdj, SuperStructureConstants.WRIST_CUBE_HYRBID.plus(wristAdj));
                 Drive.getInstance().changeMax();
                 break;
+
             case STOWED:
                 Drive.getInstance().changeMax();
                 stowAll();
                 break;
+
             case CONE_INTAKE:
                 Drive.getInstance().changeMax();
                 intakeCone(SuperStructureConstants.ELEVATOR_INTAKE_CONE_UPRIGHT + elevAdj, SuperStructureConstants.WRIST_INTAKE_CONE_UPRIGHT.plus(wristAdj));
                 break;
+
             case CUBE_INTAKE:
                 Drive.getInstance().changeMax();    
                 intakeCube(SuperStructureConstants.ELEVATOR_INTAKE_CUBE + elevAdj, SuperStructureConstants.WRIST_INTAKE_CUBE.plus(wristAdj));
                 break;
+
             case FALLEN_CONE:
                 Drive.getInstance().changeMax();
                 intakeCone(SuperStructureConstants.ELEVATOR_INTAKE_CONE_FALLEN + elevAdj, SuperStructureConstants.WRIST_INTAKE_CONE_FALLEN.plus(wristAdj));//SuperStructureConstants.WRIST_INTAKE_CONE_FALLEN);
                 break;
+
             case OPEN_DOOR:
                 goToPosition(SuperStructureConstants.ELEVATOR_INTAKE_CONE_FALLEN + elevAdj, SuperStructureConstants.WRIST_INTAKE_CONE_FALLEN.plus(wristAdj));
                 break;
+
             case CUSTOM:
                 goToPosition(SmartDashboard.getNumber("custom elevator", 0), Rotation2d.fromDegrees(SmartDashboard.getNumber("custom wrist", -55)));
                 Drive.getInstance().changeSlow();
                 break;
+
             case SEAGUL:
-                //goToPosition(0, Rotation2d.fromDegrees(-20));
                 intake(SuperStructureConstants.ELEVATOR_INTAKE_SEAGUL + elevAdj, SuperStructureConstants.WRIST_INTAKE_SEAGUL.plus(wristAdj), IntakeState.INTAKE_CONE_SEAGUL);
                 Drive.getInstance().changeSlow();
                 break;
+
             case PORTAL:
                 intake(SuperStructureConstants.ELEVATOR_INTAKE_PORTAL, SuperStructureConstants.WRIST_INTAKE_PORTAL.plus(wristAdj), IntakeState.INTAKE_CONE);
                 Drive.getInstance().changeVerySlow();
                 break;
+
             case ZERO:
                 zeroAll();
                 break;
+
             case CONE_SNIPER:
                 Drive.getInstance().changeMax();
                 goToPosition(SuperStructureConstants.ELEVATOR_CONE_SNIPER + elevAdj, SuperStructureConstants.WRIST_CONE_SNIPER.plus(wristAdj));
                 break;
+
             case CLIMB:
                 Intake.getInstance().closeJaws();
                 goToPosition(0, Rotation2d.fromDegrees(0));
                 break;
+
             default:
                 break;
         }
@@ -325,7 +351,6 @@ public class SuperStructure extends SubsystemBase {
 
     public void setIdleIntake(boolean idle){
         idleIntake = idle;
-
     }
 
     public void enableIdling(){
